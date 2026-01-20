896ed70493b01577d5e6a499a2af0310d5e43ef1
gus <gus_88i@outlook.com>
2026-01-20 13:54:09 +0200

    Added meaningfull and project-specific names to resources

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,46 +10,46 @@ resource "aws_vpc" "healthcare_vpc" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "healthcare_public_subnet" {
   vpc_id                  = aws_vpc.healthcare_vpc.id
   cidr_block              = "10.0.101.0/24"
   map_public_ip_on_launch   = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "healthcare-public-subnet"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "healthcare_private_subnet_a" {
   vpc_id            = aws_vpc.healthcare_vpc.id
   cidr_block        = "10.0.102.0/24"
   availability_zone = "af-south-1a"
 
   tags = {
-    Name = "private-subnet-a"
+    Name = "healthcare-private-subnet-a"
   }
 }
 
-resource "aws_subnet" "private_subnet_b" {
+resource "aws_subnet" "healthcare_private_subnet_b" {
   vpc_id            = aws_vpc.healthcare_vpc.id
   cidr_block        = "10.0.103.0/24"
   availability_zone = "af-south-1b"
 
   tags = {
-    Name = "private-subnet-b"
+    Name = "healthcare-private-subnet-b"
   }
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "db-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.private_subnet_b.id]
+  name       = "healthcare-db-subnet-group"
+  subnet_ids = [aws_subnet.healthcare_private_subnet_a.id, aws_subnet.healthcare_private_subnet_b.id]
 
   tags = {
-    Name = "db-subnet-group"
+    Name = "healthcare-db-subnet-group"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "healthcare_igw" {
   vpc_id = aws_vpc.healthcare_vpc.id
 
   tags = {
@@ -57,20 +57,20 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "healthcare_public_rt" {
   vpc_id = aws_vpc.healthcare_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.healthcare_igw.id
   }
 
   tags = {
-    Name = "public-route-table"
+    Name = "healthcare-public-route-table"
   }
 }
 
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "healthcare_public_association" {
+  subnet_id      = aws_subnet.healthcare_public_subnet.id
+  route_table_id = aws_route_table.healthcare_public_rt.id
 }
