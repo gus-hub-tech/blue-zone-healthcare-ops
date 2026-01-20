71bca5fac40a9172e05a647c446e622e6e88691b
gus <gus_88i@outlook.com>
2026-01-20 14:07:14 +0200

    Fixed output.tf refrences to aling with resources defined in my network.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.healthcare_vpc.id
+  value       = aws_vpc.healthcare_vpc.id
   description = "The ID of the VPC"
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+  value       = aws_subnet.healthcare_public_subnet.id
   description = "The ID of the public subnet"
 }
 
 output "db_endpoint" {
-  value = aws_db_instance.patient_db.endpoint
+  value       = aws_db_instance.patient_db.endpoint
   description = "The endpoint of the PostgreSQL database"
 }
 
 output "db_arn" {
-  value = aws_db_instance.patient_db.arn
+  value       = aws_db_instance.patient_db.arn
   description = "The ARN of the PostgreSQL database"
 }
