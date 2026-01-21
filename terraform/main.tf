2e016970f3b1c5d7651e0597dc34bce21c151184
gus <gus_88i@outlook.com>
2026-01-21 14:22:59 +0200

    Fetching the latest ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,17 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.healthcare_public_subnet.id
-  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  vpc_security_group_ids = [aws_security_group.app-server_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
